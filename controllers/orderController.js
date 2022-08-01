dbf39e939c5cb43ec9dc2ab7a7150d9dd7ad0165
tdnine <adityanr4@gmail.com>
2022-08-01 13:03:31 +0530

    updated tracking details fetch function

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -788,8 +788,8 @@ exports.trackDetails = async(req, res, next) => {
     // -------- GET TRACKING DATA FROM API IF VENDOR ID EXISTS -------------------- //
     
         if(order.vendorId && order.vendorId != 0){   
-            //CHECK FOR SHIPWAY AND SHIPWAY VENDOR ID, ELSE GO TO LINKED//                  
-            if(adminUser.trackingType=='shipway' || linkedVendorArray.map(item => item.id).indexOf(order.vendorId) == -1){
+            //CHECK FOR SHIPWAY AND SHIPWAY VENDOR ID, ELSE GO TO LINKED | adminUser.trackingType=='linked' || adminUser.trackingType=='shipway' &&  //                  
+            if(linkedVendorArray.map(item => item.id).indexOf(order.vendorId) == -1){
                 //debug('shipway tracking')
                 let excludeArr = ['914653', '9146530', '920193', '557740445', '1614352', '3779137', '3734458', '4570177', '3106960',
                 '527471', '3981373', '3431059', '3250406', '6172115', '8265503']
@@ -838,7 +838,7 @@ exports.trackDetails = async(req, res, next) => {
                 }else{                
                     res.json({status: 'fail'})
                 }
-            }else if(adminUser.trackingType=='linked'){
+            }else if(linkedVendorArray.map(item => item.id).includes(order.vendorId)){
                 //debug('linked tracking')
                 let link = getLink(order)
                 res.status(200).json({order, link, status:'Success'})
